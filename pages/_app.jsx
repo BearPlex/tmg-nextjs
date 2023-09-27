374c27fc1951a0b994c4a0c098f16842f0655a47
Faizan-Ahmad <61558827+FaizanAhmad-mern-dev@users.noreply.github.com>
2023-09-27 18:22:25 +0500

    dangerouslySetInnerHTML for GTM

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,7 +9,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head></Head>
-      <Script id="gtm-script">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NP537VKZ');`,
+        }}
+      ></Script>
+      {/* <Script id="gtm-script">
         {`
             (function(w,d,s,l,i){
               w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
@@ -20,7 +27,7 @@ function MyApp({ Component, pageProps }) {
               f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NP537VKZ');
           `}
-      </Script>
+      </Script> */}
       <Component {...pageProps} />;
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
-class MyDocument extends Document {
+import Script from "next/script";
+class WebDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="en-US">
+        <Head>
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NP537VKZ');`,
+            }}
+          ></Script>
+        </Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NP537VKZ"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
@@ -22,5 +31,4 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
+export default WebDocument;
