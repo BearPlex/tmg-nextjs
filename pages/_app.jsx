8e64945a5dc05b7271fbac1de81bdf946964f426
Faizan-Ahmad <61558827+FaizanAhmad-mern-dev@users.noreply.github.com>
2023-09-27 19:14:06 +0500

    also adding Route in app for page change

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,32 @@
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Script from "next/script";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.dataLayer &&
+        window.dataLayer.push({
+          event: "pageview",
+          page: url,
+        });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
+  // ... rest of your code
+
   return (
     <>
       <Head></Head>
@@ -16,21 +37,13 @@ function MyApp({ Component, pageProps }) {
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NP537VKZ');`,
         }}
       ></Script>
-      {/* <Script id="gtm-script">
-        {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NP537VKZ');
-          `}
-      </Script> */}
       <Component {...pageProps} />;
     </>
   );
 }
 
 export default MyApp;
+
+// function MyApp({ Component, pageProps }) {
+
+// should i add this inside _document or _app in next js
