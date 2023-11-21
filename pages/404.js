ae79bda9997b6370354210f9f305e0216f6105c2
Faizan-Ahmad <61558827+FaizanAhmad-mern-dev@users.noreply.github.com>
2023-11-21 19:27:48 +0500

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you're looking for does not exist.</p>
+      <Link href="/">
+        <a>Go back to the homepage</a>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
